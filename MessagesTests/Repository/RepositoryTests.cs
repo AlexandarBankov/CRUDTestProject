bd50b220b50ef79f79e24918d001d4856caf038a
abankov <abankov@>
2025-01-16 17:52:32 +0200

    completed the message repository tests

--- MessagesTests/Repository/RepositoryTests.cs
+++ MessagesTests/Repository/RepositoryTests.cs
@@ -20,7 +20,9 @@ namespace MessagesTests.Repository
 
             // Insert seed data into the database using one instance of the context
             context = new ApplicationDbContext(options);
-            
+
+            context.RemoveRange(context.Messages);
+
             context.Messages.Add(message);
             context.SaveChanges();
 
@@ -39,6 +41,44 @@ namespace MessagesTests.Repository
         public void DeleteMissing()
         {
             Assert.Throws<NotFoundException>(() => repository.Delete(missingId));
+        }
+
+        [Fact]
+        public void UpdateExisting()
+        {
+            const string updatedContent = "upContent";
+            const string updatedName = "upName";
+            var result = repository.Update(message.Id, updatedName, updatedContent);
+
+            Assert.Equal(updatedName, result.Name);
+            Assert.Equal(updatedContent, result.Content);
+            Assert.Equal(updatedName, context.Messages.Find(result.Id).Name);
+            Assert.Equal(updatedContent, context.Messages.Find(result.Id).Content);
+        }
+
+        [Fact]
+        public void UpdateMissing()
+        {
+            const string updatedContent = "upContent";
+            const string updatedName = "upName";
+            
+            Assert.Throws<NotFoundException>(() => repository.Update(missingId, updatedName, updatedContent));
+        }
+
+        [Fact]
+        public void GetPosterUsernameByExistingId()
+        {
+            var result = repository.GetPosterUsernameById(message.Id);
+
+            Assert.Equal(message.Username, result);
+        }
+
+        [Fact]
+        public void GetPosterUsernameByMissingId()
+        {
+            var result = repository.GetPosterUsernameById(missingId);
+
+            Assert.Null(result);
         }
     }
 }
