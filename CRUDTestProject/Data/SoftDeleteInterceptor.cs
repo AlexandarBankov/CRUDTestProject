bc3820be0a70d8268fbe43656f8039abf3c0d766
abankov <abankov@>
2025-01-21 16:33:11 +0200

    added integration tests for restoring and seeing soft deleted messages attempting to delete a soft deleted message now deletes it fully

--- CRUDTestProject/Data/SoftDeleteInterceptor.cs
+++ CRUDTestProject/Data/SoftDeleteInterceptor.cs
@@ -16,6 +16,8 @@ namespace CRUDTestProject.Data
                 .Entries<ISoftDeletable>()
                 .Where(e => e.State == EntityState.Deleted))
             {
+                if (entry.Entity.IsDeleted) continue;
+
                 entry.State = EntityState.Modified;
                 entry.Entity.IsDeleted = true;
                 entry.Entity.DeletedOn = DateTime.Now;
